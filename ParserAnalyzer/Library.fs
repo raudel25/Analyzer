da9393688de3e4336a0f77c48988b3c56bffb353
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-05-20 15:33:00 -0400

    tester

---
+++ ParserAnalyzer/Library.fs
@@ -0,0 +1,34 @@
+﻿namespace ParserAnalyzer
+
+open System
+
+module Parser =
+    type ParseResult<'a> =
+        | Success of 'a
+        | Failure of string
+
+    type Parser<'T> = Parser of (string -> ParseResult<'T * string>)
+
+    let run parser input =
+        let (Parser innerFn) = parser
+
+        innerFn input
+
+    let pChar charToMath =
+        let innerFn str =
+            if String.IsNullOrEmpty str then
+
+                Failure "No more input"
+            else
+                let first = str[0]
+
+                if first = charToMath then
+                    let remaining = str[1..]
+
+                    Success(charToMath, remaining)
+                else
+                    let msg = $"Excepting '%c{charToMath}'. Got '%c{first}'"
+
+                    Failure msg
+
+        Parser innerFn
---
+++ Tester/Program.fs
@@ -0,0 +1,8 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open ParserAnalyzer
+let parseA = Parser.pChar 'A'
+let inputABC = "ABC"
+let parseResult = Parser.run parseA inputABC
+let inputZBC = "ZBC"
+let run = Parser.run parseA inputZBC
