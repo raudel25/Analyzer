1a362d2ed08afa65a7f441dfe0aa5fd6b393b1f4
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-05-20 15:44:47 -0400

    basic

--- ParserAnalyzer/Library.fs
+++ ParserAnalyzer/Library.fs
@@ -32,3 +32,44 @@ module Parser =
                     Failure msg
 
         Parser innerFn
+
+    let andThen parser1 parser2 =
+        let innerFn input =
+            let result1 = run parser1 input
+
+            match result1 with
+            | Success (value1, remaining1) ->
+                let result2 = run parser2 remaining1
+
+                match result2 with
+                | Success (value2, remaining2) ->
+                    let newValue = (value1, value2)
+
+                    Success(newValue, remaining2)
+
+                | Failure err -> Failure err
+
+            | Failure err -> Failure err
+
+        Parser innerFn
+
+    let (.>>.) = andThen
+
+    let orElse parser1 parser2 =
+        let innerFn input =
+            let result1 = run parser1 input
+
+            match result1 with
+            | Success _ -> result1
+            | Failure _ ->
+                let result2 = run parser2 input
+
+                result2
+
+        Parser innerFn
+
+    let (<|>) = orElse
+
+    let choice listOfParsers = List.reduce (<|>) listOfParsers
+
+    let anyOf listOfChars = listOfChars |> List.map pChar |> choice
--- Tester/Program.fs
+++ Tester/Program.fs
@@ -1,8 +1,20 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
 open ParserAnalyzer
+open ParserAnalyzer.Parser
+
 let parseA = Parser.pChar 'A'
 let inputABC = "ABC"
 let parseResult = Parser.run parseA inputABC
 let inputZBC = "ZBC"
 let run = Parser.run parseA inputZBC
+
+let parseLowercase = anyOf [ 'a' .. 'z' ]
+
+let x = Parser.run parseLowercase "aBC" // Success ('a', "BC")
+let y = Parser.run parseLowercase "ABC" // Failure "Expecting 'z'. Got 'A'"
+
+let charFunc =
+    match y with
+    | Success (a, b) -> printfn $"success %c{a} %s{b}"
+    | Failure _ -> printf "err"
