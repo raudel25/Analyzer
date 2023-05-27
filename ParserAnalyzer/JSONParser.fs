61d855ad4e0c069cf1b0e349a66052b15006314f
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-05-27 15:30:34 -0400

    JSON String

--- ParserAnalyzer/JSONParser.fs
+++ ParserAnalyzer/JSONParser.fs
@@ -1,6 +1,7 @@
 namespace ParserAnalyzer
 
 open Analyzer
+open System
 
 type JValue =
     | JString of string
@@ -11,6 +12,50 @@ type JValue =
     | JArray of JValue list
 
 module JSON =
-    let jNull =
-        pString "null" |>> (fun _ -> JNull) 
-        <?> "null"
+    let (>>%) p x = p |>> (fun _ -> x)
+
+    let jNull = pString "null" >>% JNull <?> "null"
+
+    let jBool =
+        let jTrue = pString "true" >>% JBool true
+        let jFalse = pString "false" >>% JBool false
+
+        jTrue <|> jFalse <?> "bool"
+
+    let jUnEscapedChar =
+        let label = "char"
+        satisfy (fun ch -> ch <> '\\' && ch <> '\"') label
+
+    let jEscapedChar =
+        [ ("\\\"", '\"')
+          ("\\\\", '\\')
+          ("\\/", '/')
+          ("\\b", '\b')
+          ("\\f", '\f')
+          ("\\n", '\n')
+          ("\\r", '\r')
+          ("\\t", '\t') ]
+
+        |> List.map (fun (toMatch, result) -> pString toMatch >>% result)
+        |> choice
+        <?> "escaped char"
+
+    let jUnicodeChar =
+        let backslash = pChar '\\'
+        let uChar = pChar 'u'
+        let hexDigit = anyOf ([ '0' .. '9' ] @ [ 'A' .. 'F' ] @ [ 'a' .. 'f' ])
+        let fourHexDigits = hexDigit .>>. hexDigit .>>. hexDigit .>>. hexDigit
+
+        let convertToChar (((h1, h2), h3), h4) =
+            let str = $"%c{h1}%c{h2}%c{h3}%c{h4}"
+            Int32.Parse(str, Globalization.NumberStyles.HexNumber) |> char
+
+        backslash >>. uChar >>. fourHexDigits |>> convertToChar
+
+    let quotedString =
+        let quote = pChar '\"' <?> "quote"
+        let jChar = jUnEscapedChar <|> jEscapedChar <|> jUnicodeChar
+
+        quote >>. manyChars jChar .>> quote
+
+    let jString = quotedString <?> "quoted string" |>> JString
--- Tester/Program.fs
+++ Tester/Program.fs
@@ -1,1 +1,21 @@
-﻿open ParserAnalyzer
+﻿open ParserAnalyzer.JSON
+open ParserAnalyzer.Analyzer
+
+run jBool "true" |> printResult
+// Success: JBool true
+
+run jBool "false" |> printResult
+// Success: JBool false
+
+run jBool "truX" |> printResult
+
+run jString "\"\"" |> printResult
+  // Success (JString "")
+run jString "\"a\""|> printResult
+  // Success (JString "a")
+run jString "\"ab\""|> printResult
+  // Success (JString "ab")
+run jString "\"ab\\tde\""|> printResult
+  // Success (JString "ab\tde")
+run jString "\"ab\\u263Ade\""|> printResult
+  // Success (JString "ab☺de")
